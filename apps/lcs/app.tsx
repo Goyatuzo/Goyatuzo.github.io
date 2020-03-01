1510a04de62e0b17624b66c78caf1b1d5d0ce419
Yuto Otaguro <yotagur@gmail.com>
2020-03-01 15:02:40 -0600

    React app binds properly

---
+++ apps/lcs/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const LCSApp: React.StatelessComponent<{}> = props => {
+    return (
+        <div>TESTING</div>
+    )
+}
+
+export default LCSApp;
---
+++ apps/lcs/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import LCSApp from './app';
+
+render(<LCSApp />, document.getElementById('lcs-app'));
