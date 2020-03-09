4ab21f8a0ba9c991b54088df9292f752872c6663
Yuto Otaguro <yotagur@gmail.com>
2020-03-09 12:48:06 -0500

    Create fetch function for all 3 data pieces

--- apps/corona/redux/actions/index.ts
+++ apps/corona/redux/actions/index.ts
@@ -30,6 +30,16 @@ function requestRecoveredData(): CrnTableAction {
     }
 }
 
+function storeRecoveredData(value, headers: string[]): CrnTableAction {
+    return {
+        type: CrnTableActionType.STORE_RECOVERED,
+        value: {
+            value,
+            headers
+        }
+    }
+}
+
 function requestDeathsData(): CrnTableAction {
     return {
         type: CrnTableActionType.REQUEST_DEATHS,
@@ -37,20 +47,50 @@ function requestDeathsData(): CrnTableAction {
     }
 }
 
-export function fetchConfirmedData() {
-    return (dispatch: ThunkDispatch<any, any, CrnTableAction>, getState: () => CrnTableState) => {
-        dispatch(requestConfirmedData());
+function storeDeathsData(value, headers: string[]): CrnTableAction {
+    return {
+        type: CrnTableActionType.STORE_DEATHS,
+        value: {
+            value,
+            headers
+        }
+    }
+}
 
-        return axios.get<string>(`${rootUrl}time_series_19-covid-Confirmed.csv`).then(response => {
+function fetchData(preFetchDispatch: () => CrnTableAction, storeDispatch: (value, headers: string[]) => CrnTableAction, url: string) {
+    return (dispatch: ThunkDispatch<any, any, CrnTableAction>, getState: () => CrnTableState) => {
+        dispatch(preFetchDispatch());
+
+        return axios.get<string>(url).then(response => {
             // const datas = response.data.split('\n');
 
             csvToJson({ output: 'csv' }).fromString(response.data).then(data => {
                 let headers = response.data.split('\n')[0].split(',');
 
-                dispatch(storeConfirmedData(data, headers));
+                dispatch(storeDispatch(data, headers));
             });
         }).catch(err => {
             console.error(err);
         });
+    }
+}
+
+export function fetchConfirmedData() {
+    return fetchData(requestConfirmedData, storeConfirmedData, `${rootUrl}time_series_19-covid-Confirmed.csv`);
+}
+
+export function fetchDeathsData() {
+    return fetchData(requestDeathsData, storeDeathsData, `${rootUrl}time_series_19-covid-Deaths.csv`)
+}
+
+export function fetchRecoveredData() {
+    return fetchData(requestDeathsData, storeDeathsData, `${rootUrl}time_series_19-covid-Recovered.csv`)
+}
+
+export function fetchAllData() {
+    return (dispatch: ThunkDispatch<any, any, CrnTableAction>) => {
+        dispatch(fetchConfirmedData());
+        dispatch(fetchDeathsData());
+        dispatch(fetchRecoveredData());
     }
 }
--- apps/corona/redux/index.ts
+++ apps/corona/redux/index.ts
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
-import { fetchConfirmedData } from './actions'
+import { fetchAllData } from './actions'
 
 const store = createStore(reducer, applyMiddleware(thunk));
-store.dispatch(fetchConfirmedData() as any);
+store.dispatch(fetchAllData() as any);
 
 export default store;
