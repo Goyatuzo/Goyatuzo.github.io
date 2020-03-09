0bbce54a6ea5f7d5c45332f98c8a3d9126ca44ae
Yuto Otaguro <yotagur@gmail.com>
2020-03-09 15:30:07 -0500

    Graph plots desired data

--- apps/corona/react/historic-graph.tsx
+++ apps/corona/react/historic-graph.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { CrnTableState } from '../redux/reducers';
 import { CrnLocation } from '../classes/location';
-import Chart = require('chart.js');
+import { Chart } from 'chart.js';
 
 interface StateToProps {
     data: CrnLocation[];
@@ -20,6 +20,62 @@ export class CoronaHistoricGraphComponent extends React.PureComponent<GraphProps
         this.canvasRef = React.createRef<HTMLCanvasElement>();
     }
 
+    private generateDataSet(data: CrnLocation[]): Chart.ChartDataSets[] {
+        let confirmed: { t: Date, y: number }[] = [];
+        let deaths: { t: Date, y: number }[] = [];
+        let recovered: { t: Date, y: number }[] = [];
+
+        const datesInMs = Object.keys(data[0]?.statistics ?? {});
+        for (let i = 0; i < data.length; ++i) {
+            for (let j = 0; j < datesInMs.length; ++j) {
+                const stats = data[i].statistics[parseInt(datesInMs[j])];
+
+                // Add to confirmed
+                if (j >= confirmed.length) {
+                    confirmed.push({ t: new Date(stats.dateInMs), y: stats.confirmed });
+                } else {
+                    confirmed[j].y += stats.confirmed
+                }
+
+                // Add to deaths
+                if (j >= deaths.length) {
+                    deaths.push({ t: new Date(stats.dateInMs), y: stats.deaths });
+                } else {
+                    deaths[j].y += stats.deaths;
+                }
+
+                // Add to recovered
+                if (j >= recovered.length) {
+                    recovered.push({ t: new Date(stats.dateInMs), y: stats.recovered });
+                } else {
+                    recovered[j].y += stats.recovered;
+                }
+            }
+        }
+
+        console.log(confirmed.length);
+
+        return [{
+            label: 'Confirmed',
+            data: confirmed,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Deaths',
+            data: deaths,
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            borderColor: 'rgba(0, 0, 0, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Recovered',
+            data: recovered,
+            backgroundColor: 'rgba(0, 255, 0, 0.2)',
+            borderColor: 'rgba(0, 255, 0, 1)',
+            borderWidth: 1
+        }];
+    }
+
     componentDidMount() {
         this.chart = new Chart(this.canvasRef.current, {
             type: 'line',
@@ -34,27 +90,17 @@ export class CoronaHistoricGraphComponent extends React.PureComponent<GraphProps
                 }
             },
             data: {
-                datasets: [{
-                    label: 'Confirmed',
-                    data: [],
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }, {
-                    label: 'Deaths',
-                    data: [],
-                    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-                    borderColor: 'rgba(0, 0, 0, 1)',
-                    borderWidth: 1
-                }, {
-                    label: 'Recovered',
-                    data: [],
-                    backgroundColor: 'rgba(0, 255, 0, 0.2)',
-                    borderColor: 'rgba(0, 255, 0, 1)',
-                    borderWidth: 1
-                }],
+                datasets: this.generateDataSet(this.props.data)
             }
         })
+    }
+
+    componentDidUpdate() {
+        this.chart.data = {
+            datasets: this.generateDataSet(this.props.data)
+        };
+
+        this.chart.update();
     }
 
     render() {
