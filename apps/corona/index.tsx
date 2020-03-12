4509b7b02dd8c1880a67a88252bd4e6acbdff9c3
Yuto Otaguro <yotagur@gmail.com>
2020-03-12 02:29:48 -0500

    Query string  shows proper graph

--- apps/corona/index.tsx
+++ apps/corona/index.tsx
@@ -1,15 +1,17 @@
-
 import 'es6-promise';
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import store from './redux';
 import CoronaHistoricGraph from './react/historic-graph';
 
-render(<Provider store={store}>
-    <div className="ui container">
-        <h1 className="ui header">Coronavirus</h1>
-        <CoronaHistoricGraph />
-    </div>
-</Provider>, document.getElementById('corona-table-app'));
+render(<Router>
+    <Provider store={store}>
+        <div className="ui container">
+            <h1 className="ui header">Coronavirus</h1>
+            <CoronaHistoricGraph />
+        </div>
+    </Provider>
+</Router>, document.getElementById('corona-table-app'));
