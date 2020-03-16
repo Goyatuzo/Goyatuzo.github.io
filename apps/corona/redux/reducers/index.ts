65f2e91f6e54b980be27d7f17cd58f39087cab5e
Yuto Otaguro <yotagur@gmail.com>
2020-03-16 16:20:45 -0500

    No compiler errors

--- apps/corona/redux/reducers/index.ts
+++ apps/corona/redux/reducers/index.ts
@@ -1,5 +1,6 @@
 import { CrnLocation, CrnStats } from '../../classes/location';
 import { CrnTableAction, CrnTableActionType } from '../actions/actiontype';
+import { DSVRowArray } from 'd3';
 
 export interface CrnTableState {
     requestingConfirmed: boolean;
@@ -17,8 +18,11 @@ const defaultState: CrnTableState = {
     locations: []
 }
 
-function updateField(locations: CrnLocation[], data: string[][], headers: string[], fieldToUpdate: keyof CrnStats): CrnLocation[] {
+function updateField(locations: CrnLocation[], data: DSVRowArray, fieldToUpdate: keyof CrnStats): CrnLocation[] {
     let updatedLocations: CrnLocation[] = JSON.parse(JSON.stringify(locations));
+    const headers = data.columns;
+
+    console.log(headers);
 
     for (let i = 0; i < data.length; ++i) {
         const row = data[i];
@@ -31,7 +35,7 @@ function updateField(locations: CrnLocation[], data: string[][], headers: string
             statistics: {}
         };
 
-        for (let j = 4; j < data[i].length; ++j) {
+        for (let j = 4; j < headers.length; ++j) {
             const dateTokens = headers[j].split('/').map(tkn => parseInt(tkn));
 
             const date = new Date(parseInt(`20${dateTokens[2]}`), dateTokens[0] - 1, dateTokens[1]);
@@ -43,7 +47,7 @@ function updateField(locations: CrnLocation[], data: string[][], headers: string
                     recovered: 0
                 }
             }
-            
+
             if (newLocation.statistics[date.getTime()] && data[i][j] !== "") {
                 newLocation.statistics[date.getTime()][fieldToUpdate] = parseInt(data[i][j] ?? '0') || 0;
             }
@@ -69,13 +73,13 @@ export default function reducer(state = defaultState, action: CrnTableAction) {
             return { ...state, requestingRecovered: true };
         }
         case CrnTableActionType.STORE_CONFIRMED: {
-            return { ...state, requestingConfirmed: false, locations: updateField(state.locations, action.value.value, action.value.headers, "confirmed") };
+            return { ...state, requestingConfirmed: false, locations: updateField(state.locations, action.value, "confirmed") };
         }
         case CrnTableActionType.STORE_DEATHS: {
-            return { ...state, requestingDeaths: false, locations: updateField(state.locations, action.value.value, action.value.headers, "deaths") };
+            return { ...state, requestingDeaths: false, locations: updateField(state.locations, action.value, "deaths") };
         }
         case CrnTableActionType.STORE_RECOVERED: {
-            return { ...state, requestingRecovered: false, locations: updateField(state.locations, action.value.value, action.value.headers, "recovered") };
+            return { ...state, requestingRecovered: false, locations: updateField(state.locations, action.value, "recovered") };
         }
         case CrnTableActionType.SELECT_LOCATION: {
             return { ...state, chosenLocation: action.value };
