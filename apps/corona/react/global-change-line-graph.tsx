09431a368ec423837cc813d6838306580a6eba67
Yuto Otaguro <yotagur@gmail.com>
2020-03-26 01:26:57 -0500

    Update scales to reflect data

--- apps/corona/react/global-change-line-graph.tsx
+++ apps/corona/react/global-change-line-graph.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { CrnLocation } from '../classes/location';
 import { GlobalChangeLineGraphData } from '../classes/graph';
 import BaseD3Graph, { BaseGraphState } from './base-graph';
-import { select, stack, stackOffsetExpand, scaleUtc, extent, scaleLinear, area, scaleOrdinal, schemeCategory10, axisBottom, axisLeft } from 'd3';
+import { select, stack, stackOffsetExpand, scaleUtc, extent, scaleLinear, area, scaleOrdinal, schemeCategory10, axisBottom, axisLeft, max } from 'd3';
 
 interface ExternalProps {
     generateDataSet: (data: CrnLocation[]) => GlobalChangeLineGraphData;
@@ -16,8 +16,11 @@ const GlobalChangeLineGraph: React.StatelessComponent<ExternalProps> = props => 
 
         const series = props.generateDataSet(data);
 
-        const x = scaleUtc().domain(extent(series.dates)).range([state.margins.left, state.width - state.margins.right]);
-        const y = scaleLinear().range([state.height - state.margins.bottom, state.margins.top]);
+        const x = scaleUtc()
+            .domain(extent(series.dates)).range([state.margins.left, state.width - state.margins.right]);
+        const y = scaleLinear()
+            .domain([0, max(series.data, d => max(d.values))]).nice()
+            .range([state.height - state.margins.bottom, state.margins.top]);
 
         svg.append('g')
             .selectAll('path')
